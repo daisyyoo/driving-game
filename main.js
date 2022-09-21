7939f7dd3e8e9cfd2ad747223614ff53e7e0c460
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-21 22:27:47 +0000

    Completed user can start their car

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 var $page = document.querySelector('body');
-var $carTurn = document.querySelector('input');
+var $car = document.querySelector('input');
 $page.addEventListener('keydown', turnCar);
 
 function turnCar(event) {
   if (event.key === 'ArrowDown') {
-    $carTurn.className = 'car car-turn-down';
+    $car.className = 'car car-turn-down';
   } else if (event.key === 'ArrowLeft') {
-    $carTurn.className = 'car car-turn-left';
+    $car.className = 'car car-turn-left';
   } else if (event.key === 'ArrowRight') {
-    $carTurn.className = 'car car-turn-right';
+    $car.className = 'car car-turn-right';
   } else if (event.key === 'ArrowUp') {
-    $carTurn.className = 'car car-turn-up';
+    $car.className = 'car car-turn-up';
   }
 }
+
+// var stopCarID = null;
+// var stopCarID =
+
+$page.addEventListener('keydown', moveCarRight);
+
+function moveCarRight(event) {
+  if (event.keyCode === 32) {
+    // var y = $car.offsetTop;
+    for (var x = $car.offsetLeft; x < 900; x++) {
+      if (x > 0) {
+        $car.style.left = [x] + 'px';
+        // $car.style.top = y;
+      }
+    }
+  }
+}
+
+function startCar(event) {
+  moveCarRight(event);
+}
+setInterval(startCar, 16);
